436445f160c1cbe166da57e5da07bbd7b983b389
GAURAV JOSHI <gsj38@msstate.edu>
2019-08-05 20:09:01 -0500

    Updated time, space complexity and approach for the solution

--- problem1.cpp
+++ problem1.cpp
@@ -1,3 +1,17 @@
+//Time complexity: O(2^n) --> For candidate array of size N, we eventually check if it is part of combination that sums up to target {YES or NO}
+//Space complexity: O(n) --> Ommitting the result array (which is O(n^2)) we create a combination array to check if each element is part of combination sum
+//Accepted in Leetcode: Yes
+
+//Approach:
+
+/* 
+Example:
+candidate -> [2, 3, 5, 7] target -> 7
+
+1. Here, we create a backTrack method that subtracts every candidate element from target recursively until either target is 0 or less than element itself
+2. When target is 0 or less than element, we check for the next candidate until the end of the candidate array
+*/
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -8,7 +22,7 @@ class Solution{
 
     public:
 
-        vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+        vector<vector<int>> combinationSum(vector<int> &candidates, int target) 
         {
             sort(candidates.begin(), candidates.end());
             vector<int>combinations;
