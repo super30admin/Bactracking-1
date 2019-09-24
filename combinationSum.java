7563c58284c7e2bc30dbc92ddcd464555c2719ee
Ajith Balaji <ajithbalajiun@gmail.com>
2019-09-24 15:19:04 -0700

    Completed expression add problem - Please Review

--- combinationSum.java
+++ combinationSum.java
@@ -1,5 +1,5 @@
-//Time Complexity:O(N logN)
-//Space Complexity:O(N)
+//Time Complexity:O(N*2^N) where N is the size of the candidate array. N is the decision space and 2^N is exploring each decision space's different combinations. 
+//Space Complexity:O(N) 
 //In this problem, I'll be having a temp list to keep track of the intermediary results. I'll then call my backtrack function, by passing the res list, temo list, target , input and the current index as the input. If the target is less than 0, I'll simply be returning. If it is equal to 0, I'll be appending the temp list at that instant to the result list. Else, I'll iterare over the length of the input array and then add elements to the temp list and call the backtrack function recursively. After reaching the parent function, I'll be popping the last added element from the result list.
 //This code was exeuted successfully and got accepted in leetcode. 
 
