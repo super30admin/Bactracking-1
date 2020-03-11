8be370eec16181742084bf1a647f8291b186c7c2
Sai Swaroop Reddy Thammineni <54874974+saiswaroopreddy@users.noreply.github.com>
2020-03-11 11:36:10 -0700

    Expression update

--- Expression.java
+++ Expression.java
@@ -5,20 +5,24 @@ class Solution {
         return result;
     }
     
-    public void dfs(List<String> result, String num, int target, String expression, int value, int presign, int index){
+    public void dfs(List<String> result, String num, int target, String expression, long value, int presign, int index){
         if(index == num.length()){
         if(value == target){result.add(expression);}
             return;
         }
         for(int i = index;i<num.length();i++){
-            int operand = Integer.parseInt(num.substring(index, i+1));
-        if(index == 0)
+            long operand = Long.parseLong(num.substring(index, i+1));
+            if(index != i && num.charAt(index) == '0') break;
+        if(index == 0){
             dfs(result, num, target, "" + operand, operand, operand, i+1);
-            
+        }
+            else {
         //+
         dfs(result, num, target, expression +"+"+operand, value + operand, operand, i+1);
         dfs(result, num, target, expression + "-"+operand, value - operand, -1*operand, i+1);
         dfs(result, num, target, expression + "*"+operand, value - presign + operand*presign, operand*presign, i+1);
             }
+        }
     }
 }
+
