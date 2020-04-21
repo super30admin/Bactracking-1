e4ccb621b41049899e3297b9335c1834026ac75d
Akshay Shah <akshay.vjti11@gmail.com>
2020-04-20 18:33:54 -0700

    remove the redundant part and fixed the string part

--- expression_operators.py
+++ expression_operators.py
@@ -9,6 +9,12 @@ Algorithm explanation
 1. Break the string into n possible operands using recursive and backtracking on the operands
 2. Run a recursive algo wch allows to branch into all possible operators and 
 checking the evaluated output with the target at each step 
+
+Problems
+- For the test case [2,3,2], it's not considering the other path(REally thought it would)
+- Handling 0 in the middle
+
+
 """
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
@@ -31,9 +37,8 @@ class Solution:
                     operands_rec(num,i+1,n)
                     result.pop()
         
-        def expression_rec(operands,i,eval_value,prev_value,prev_string,target,n):
-            # if eval_value == target:
-            #     final_operands.append(expression[::])
+        self.result_string = ""
+        def expression_rec(operands,i,eval_value,prev_string,target,n):
             #base case
             if eval_value and eval_value > target:
                 return
@@ -46,23 +51,22 @@ class Solution:
             for op in ['+','-','*']:
                 if op == '+':
                     #eval_value = operands[i-1] + operands[i]
-                    eval_value = prev_value + operands[i]
+                    eval_value = eval_value + operands[i]
                 elif op == '-':
                     #eval_value = operands[i-1] - operands[i]
-                    eval_value = prev_value - operands[i]
+                    eval_value = eval_value - operands[i]
                 elif op == '*':
                     #eval_value = operands[i-1] * operands[i]
-                    eval_value = prev_value * operands[i]
-                result_string = prev_string + op + str(operands[i])
-                expression.append(result_string)
-                expression_rec(operands,i+1,eval_value,eval_value,result_string,target,n)
-                expression.pop()
+                    eval_value = eval_value * operands[i]
+                self.result_string = prev_string + op + str(operands[i])
+                expression_rec(operands,i+1,eval_value,self.result_string,target,n)
+                self.result_string = prev_string
         
         final_operands = []
         expression = []
         operands_rec(num,0,len(num))
         print(operands)
         for op in operands:
-            expression_rec(op,1,None,op[0],str(op[0]),target,len(op))
+            expression_rec(op,1,op[0],str(op[0]),target,len(op))
         
         return final_operands
