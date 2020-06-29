ea20c9d3a95877ff92ff3810f5e0a67b5eef7a30
chirag <cr.tagadiya@gmail.com>
2020-06-29 19:20:56 -0400

    recursive solution

--- Combination_SUM.java
+++ Combination_SUM.java
@@ -37,9 +37,57 @@ public class Combination_SUM {
             backtrack(candidates, temp, sum+ candidates[i],i);
             
             // backtrack
+ 
+ 
             temp.remove(temp.size() -1 );
         }
     }
 
 
 }
+
+/*
+    You choose it or not and then maintain new list at each recursion
+
+    Using recursion
+
+    class Solution {
+    int targ;
+    List<List<Integer>> result;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        targ = target;
+        result =  new ArrayList();
+        
+        helper( candidates, new ArrayList(),0,0);
+        return result;
+    }
+    
+    private void helper(int[] candidates, List<Integer> temp, int sum, int index){
+        
+        // base case
+        
+        if( sum ==  targ){
+            // System.out.println("index : "+index +"temp "+temp.toString());
+            result.add(temp);
+            return;
+        }
+        
+        if( sum > targ || index ==  candidates.length) {
+            // System.out.println("hey"+sum);
+            return;}
+        
+        // logic
+        
+        //  if you don't choose move to the next element
+        helper( candidates, new ArrayList(temp), sum, index + 1);
+        
+        // System.out.println("index : "+index + " element "+ candidates[index]); 
+        // if you choose
+        temp.add(candidates[index]);
+        
+        helper(candidates, new ArrayList(temp), sum + candidates[index], index);
+    }
+}
+
+
+*/
