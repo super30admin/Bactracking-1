6f0f7fd69864b19524bb6b2ec8ac4216e0e2a988
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-08-11 18:27:17 -0500

    39. Combination Sum: Find all unique combinations in candidates where the candidate numbers sums to target

---
+++ src/Combination_Sum.java
@@ -0,0 +1,88 @@
+import java.util.*;
+/********************************************Using Backtracking*******************************************/
+//Time Complexity : O(2^n),  n is the length of the candidates array.
+//Space Complexity : O(n)
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
+
+public class Combination_Sum {
+	static List<List<Integer>> result;
+	static int targ;
+	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+		result = new ArrayList<>();
+		targ = target;
+		//edge case
+		if(candidates == null || candidates.length == 0)
+			return result;
+
+		backtrack(candidates, new ArrayList<>(), 0, 0);	//calling function on the given array, using a temp list, sum and index to track the candidates array
+		return result;
+	}
+
+	private static void backtrack(int[] candidates, List<Integer> temp, int sum, int index){
+		//base
+		if(sum > targ || index >= candidates.length)
+			return;
+
+		if(sum == targ){
+			result.add(new ArrayList<>(temp));	// add the copy of temp till the point to result
+			return;
+		}
+
+		//logic
+		for(int i=index; i<candidates.length; i++){
+			//action
+			temp.add(candidates[i]);
+
+			//recurse
+			backtrack(candidates, temp, sum + candidates[i], i);	//calling recursive function with increasing sum, new temp and on index as i
+
+			//backtrack
+			temp.remove(temp.size()-1);	//return back to one step back when base case hits, to traverse other possible paths
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] candidates = {2,3,6,7};
+		int target = 7;
+		System.out.println(combinationSum(candidates, target));
+	}
+}
+
+/********************************************Using Recursion*******************************************/
+//Time Complexity : O(2^n),  n is the length of the candidates array
+//Space Complexity : O(2^n *n), as a new list created at each recursive call
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
+
+class Combination_Sum_Recurse {
+	List<List<Integer>> result;
+	int targ;
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		result = new ArrayList<>();
+		targ = target;
+		if(candidates == null || candidates.length == 0)
+			return result;
+
+		helper(candidates, new ArrayList<>(), 0, 0);
+		return result;
+	}
+
+	private void helper(int[] candidates, List<Integer> temp, int sum, int index){
+		//base
+		if(sum > targ || index >= candidates.length)
+			return;
+
+		if(sum == targ){
+			result.add(temp);
+			return;
+		}
+		//logic
+		//dont choose
+		helper(candidates, new ArrayList<>(temp), sum, index + 1);	// new copy of temp is passed so that we don't loose the current state
+
+		//choose
+		temp.add(candidates[index]);
+		helper(candidates, new ArrayList<>(temp), sum + candidates[index], index); 	 //sum is increased by choosed value, new copy of temp is passed so that we don't loose the current state
+	}
+}
