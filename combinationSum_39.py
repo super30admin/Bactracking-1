5077edbad95960245e29b5e1cb76a382e10f2796
vatsaashwin <aashwinvats@GMAIL.com>
2020-09-22 17:37:05 -0700

    Completed Combination Sum

--- combinationSum_39.py
+++ combinationSum_39.py
@@ -4,3 +4,37 @@
 # Any problem you faced while coding this : No
 
 # Your code here along with comments explaining your approach
+
+class Solution:
+    
+    def __init__(self):
+        self.output = []
+    
+    
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        path = []
+        self.backTracking(candidates, target, 0 , path)
+        
+        return self.output
+    
+    def backTracking(self, candidates, target, index, path):
+        
+        if target < 0:
+            return
+        elif target == 0:
+            self.output.append(list(path))
+            return
+        elif index >= len(candidates):
+            return
+        
+        for i in range(index, len(candidates)):
+            
+            #add to current path
+            path.append(candidates[i])
+            
+            #recursive calls
+            self.backTracking(candidates, target-candidates[i], i, path)
+            
+            #delete from current path
+            path.pop(len(path)-1)
