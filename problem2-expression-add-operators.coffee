61655110650f48e3871e375af6ea118af21c8bc3
Ken Ore <v-kore@expediagroup.com>
2020-09-22 19:30:24 -0500

    problem2 passing some test cases on leetcode, still has a bug

--- problem2-expression-add-operators.coffee
+++ problem2-expression-add-operators.coffee
@@ -1,11 +1,20 @@
 #https://leetcode.com/problems/expression-add-operators/
-#// Time Complexity :
-#// Space Complexity :
-#// Did this code successfully run on Leetcode :
+#// Time Complexity : O(N^N) basically visit all combinations of the nums and operators
+#// Space Complexity : O(N^N) stack usage
+#// Did this code successfully run on Leetcode : base case yes, not all test cases I still have a bug somewhere
 #// Any problem you faced while coding this :
 #
+# knowing the difference between substr and substring!!!  (indices vs index+length!!!!!)
 #
 #// Your code here along with comments explaining your approach
+#
+# traverse the nums input from length [1, nums.length)
+# at each recursive level
+#   recurse on the 3 operators +, -, * calculating the sums and expr for each
+#   if you have reached the end of indices within nums
+#      store the expr to output IF it results in the target
+#
+# return the output array
 
 #/**
 # * @param {string} num
@@ -20,23 +29,29 @@ addOperators = (nums, target) ->
       output.push(expr) if prevSum is target
       return
 
-    curr = parseInt(nums[index])
+    for i in [index...nums.length]
+      curNum = nums.substring(index, i + 1)
+      curr = parseInt(curNum)
 
-    if index is 0
-      backtracking(index + 1, nums[index], curr, curr)
-    else
-      # +
-      backtracking(index + 1, expr + '+' + nums[index], prevSum + curr, curr)
+      if nums[index] is '0' and i isnt index
+        continue
 
-      # -
-      backtracking(index + 1, expr + '-' + nums[index], prevSum - curr, curr * -1)
+      if index is 0
+        backtracking(index + 1, curNum, curr, curr)
+      else
+        # +
+        backtracking(index + 1, "#{expr}+#{curNum}", prevSum + curr, curr)
 
-      # *
-      backtracking(index + 1, expr + '*' + nums[index],
-        prevSum - prevAddition + prevAddition * curr, prevAddition * curr)
+        # -
+        backtracking(index + 1, "#{expr}-#{curNum}", prevSum - curr, curr * -1)
+
+        # *
+        backtracking(index + 1, "#{expr}*#{curNum}",
+          prevSum - prevAddition + prevAddition * curr, prevAddition * curr)
 
   backtracking(0, '', 0, 0)
 
   output
 
-addOperators("123", 6)
+addOperators("12", 3)
+#addOperators("123", 6)
--- problem2-expression-add-operators.js
+++ problem2-expression-add-operators.js
@@ -1,10 +1,20 @@
 //https://leetcode.com/problems/expression-add-operators/
-//// Time Complexity :
-//// Space Complexity :
-//// Did this code successfully run on Leetcode :
+//// Time Complexity : O(N^N) basically visit all combinations of the nums and operators
+//// Space Complexity : O(N^N) stack usage
+//// Did this code successfully run on Leetcode : base case yes, not all test cases I still have a bug somewhere
 //// Any problem you faced while coding this :
 
+// knowing the difference between substr and substring!!!  (indices vs index+length!!!!!)
+
 //// Your code here along with comments explaining your approach
+
+// traverse the nums input from length [1, nums.length)
+// at each recursive level
+//   recurse on the 3 operators +, -, * calculating the sums and expr for each
+//   if you have reached the end of indices within nums
+//      store the expr to output IF it results in the target
+
+// return the output array
 
 ///**
 // * @param {string} num
@@ -17,29 +27,39 @@ addOperators = function(nums, target) {
   var backtracking, output;
   output = [];
   backtracking = function(index, expr, prevSum, prevAddition) {
-    var curr;
+    var curNum, curr, i, j, ref, ref1, results;
     if (index >= nums.length) {
       if (prevSum === target) {
         output.push(expr);
       }
       return;
     }
-    curr = parseInt(nums[index]);
-    if (index === 0) {
-      return backtracking(index + 1, nums[index], curr, curr);
-    } else {
-      // +
-      backtracking(index + 1, expr + '+' + nums[index], prevSum + curr, curr);
-      // -
-      backtracking(index + 1, expr + '-' + nums[index], prevSum - curr, curr * -1);
-      // *
-      return backtracking(index + 1, expr + '*' + nums[index], prevSum - prevAddition + prevAddition * curr, prevAddition * curr);
+    results = [];
+    for (i = j = ref = index, ref1 = nums.length; (ref <= ref1 ? j < ref1 : j > ref1); i = ref <= ref1 ? ++j : --j) {
+      curNum = nums.substring(index, i + 1);
+      curr = parseInt(curNum);
+      if (nums[index] === '0' && i !== index) {
+        continue;
+      }
+      if (index === 0) {
+        results.push(backtracking(index + 1, curNum, curr, curr));
+      } else {
+        // +
+        backtracking(index + 1, `${expr}+${curNum}`, prevSum + curr, curr);
+        // -
+        backtracking(index + 1, `${expr}-${curNum}`, prevSum - curr, curr * -1);
+        // *
+        results.push(backtracking(index + 1, `${expr}*${curNum}`, prevSum - prevAddition + prevAddition * curr, prevAddition * curr));
+      }
     }
+    return results;
   };
   backtracking(0, '', 0, 0);
   return output;
 };
 
-addOperators("123", 6);
+addOperators("12", 3);
+
+//addOperators("123", 6)
 
 //# sourceMappingURL=problem2-expression-add-operators.js.map
