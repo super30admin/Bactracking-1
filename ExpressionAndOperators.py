8025917e5bc59380e85e35b86d54cbe3fa5d818b
aakanksha_13 <>
2020-11-18 18:22:29 +0530

    Completed ExpressionAndOperators and changes combinationSum SC

--- ExpressionAndOperators.py
+++ ExpressionAndOperators.py
@@ -1,7 +1,50 @@
-#Time Complexity : O()
-#Space Complexity : O()
+#Time Complexity : O(4^n) where n is the length of input string
+#Space Complexity : O(n)
 #Did this code successfully run on Leetcode : Yes
 #Three line explanation of solution in plain english:
 
 class Solution:
+    def helper(self, num, temp, tail, currTotal, index):
+        if index == len(num):
+            if currTotal == self.target:
+                self.result.append(copy.deepcopy("".join(temp)))
+            return
+
+        for i in range(index, len(num)):
+            if index != i and num[index] == '0':
+                continue
+
+            curr = int(num[index:i+1])
+
+            if index == 0:
+                temp.append(str(curr))
+                self.helper(num, temp, curr, curr, i+1)
+                temp.pop()
+            else:
+                temp.append("+")
+                temp.append(str(curr))
+                self.helper(num, temp, curr, currTotal+curr, i+1)
+                temp.pop()
+                temp.pop()
+
+                temp.append("-")
+                temp.append(str(curr))
+                self.helper(num, temp, -curr, currTotal-curr, i+1)
+                temp.pop()
+                temp.pop()
+
+                temp.append("*")
+                temp.append(str(curr))
+                self.helper(num, temp, tail*curr, currTotal-tail+tail*curr, i+1)
+                temp.pop()
+                temp.pop()
+
     def addOperators(self, num: str, target: int) -> List[str]:
+        self.result = []
+        self.target = target
+
+        if len(num) == 0 or num is None:
+            return self.result
+
+        self.helper(num, [], 0, 0, 0)
+        return self.result
--- combinationSum.py
+++ combinationSum.py
@@ -1,9 +1,10 @@
-#Time Complexity : O(n)
-#Space Complexity : O(n)
+#Time Complexity : O(n^m) m is the target, n is the number of elements
+#Space Complexity : O(m)
 #Did this code successfully run on Leetcode : Yes
 #Three line explanation of solution in plain english: Trying all combinations and seeing if the sum of it equals to the target, if it does, we add that combination of numbers to the result array. We find all these combinations by performing backtracking and maintaining an index and a temporary array in which we add values of the elements at the current index we are maintaining. We also keep a total running sum, which is of the elements in the temporary array. If anywhere the total running sum becomes greater than the target, then we return from there, after which the index in the for loop increases by 1 and moves to the next element.
 
 class Solution:
+    #BACKTRACKING
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         if not candidates or target < 0:
             return []
@@ -12,12 +13,32 @@ class Solution:
 
         def findTarget(candidates, total, index, curr):
             if total == target:
-                self.result.append(curr)
+                self.result.append(copy.deepcopy(curr))
             elif total > target:
                 return
 
             for i in range(index, len(candidates)):
-                findTarget(candidates, total+candidates[i], i, curr+[candidates[i]])
+                curr.append(candidates[i])
+                findTarget(candidates, total+candidates[i], i, curr)
+                curr.pop()
 
         findTarget(candidates, 0, 0, [])
-        return self.result
+
+    #RECURSION
+    # def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    #     if not candidates or target < 0:
+    #         return []
+    #
+    #     self.result = []
+    #
+    #     def findTarget(candidates, total, index, curr):
+    #         if total == target:
+    #             self.result.append(curr)
+    #         elif total > target:
+    #             return
+    #
+    #         for i in range(index, len(candidates)):
+    #             findTarget(candidates, total+candidates[i], i, curr+[candidates[i]])
+    #
+    #     findTarget(candidates, 0, 0, [])
+    #     return self.result
