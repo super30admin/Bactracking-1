82de5a899269df49f29f156dc30f460224ff1f8c
Rishi Raj <rishiraj824@gmail.com>
2020-12-29 20:11:35 -0500

    Update combination-sum.java

--- combination-sum.java
+++ combination-sum.java
@@ -21,3 +21,33 @@ class Solution {
         }
     }
 }
+// Time - O(N+M)
+// space - O(N+M)
+
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if(candidates.length == 0) {
+            return result;
+        }
+        
+        helper(candidates, 0, target, 0, new ArrayList<>());
+        return result;        
+    }
+    
+    private void helper(int[] candidates, int index, int target, int curSum, List<Integer> list) {
+        
+        if(index >= candidates.length || target < curSum) {
+            return;
+        }
+        if(target == curSum) {
+            result.add(list);
+            return;
+        }
+        
+        helper(candidates, index + 1, target, curSum, new ArrayList<>(list));
+        list.add(candidates[index]);
+        helper(candidates, index, target, curSum + candidates[index], new ArrayList<>(list));            
+        
+    }
+}
