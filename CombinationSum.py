677cd40744bf1a6d091cf046e2aa6775a50bfe5b
amby2964 <75859704+amby2964@users.noreply.github.com>
2021-02-13 17:53:46 -0700

    Create CombinationSum.py

---
+++ CombinationSum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        o = []
+        
+        # backtrack solution
+        def combinationSum1(path,i,target):
+            
+            nonlocal o
+            if(target==0):
+                o.append(path[:])
+                return
+            if(i==len(candidates) or target<0):
+                return
+            
+            # choose element
+            path.append(candidates[i])
+            combinationSum1(path,i,target-candidates[i])
+        
+            # not choose
+            # backtrack
+            path.pop()
+            combinationSum1(path,i+1,target)
+        
+        
+        combinationSum1([],0,target)
+        
+        return o
