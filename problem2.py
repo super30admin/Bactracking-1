2c49b74c19a47a892db79d4a8090c9e71a1b3801
akashtanwani <77640435+akashtanwani@users.noreply.github.com>
2021-02-23 23:42:18 -0600

    initialized file

---
+++ problem2.py
@@ -0,0 +1,7 @@
+// Time Complexity : O(O(n^n))
+// Space Complexity : O(depth of recursive tree)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
