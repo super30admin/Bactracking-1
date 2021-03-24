83d1b8d6d4f7ab0a61cd5552058dad5bee411496
omkar <omkarvaidya13@gmail.com>
2021-03-23 23:13:48 -0400

    Completed Backtracking-1

---
+++ ExpressionAddOperators.java
@@ -0,0 +1,46 @@
+class Solution {
+    
+    // Time Complexity: O(N ^ 3)
+    // Space Complexity: O(N)
+    
+    private List<String> result = new ArrayList<>();
+    
+    public List<String> addOperators(String num, int target) {
+        // Edge Case Checking
+        if(num == null || num.length() == 0)
+            return result;
+        
+        backtracking(num, target, 0, 0, 0, "");
+            
+        return result;
+    }
+    
+    private void backtracking(String num, int target, int index, long prevSum, long prevAdd, String expr){
+        // Base Case
+        if(index >= num.length()){
+            if(prevSum == target){
+                result.add(expr);
+            }
+            return;
+        }
+                
+        // Logic
+        for(int i = index; i < num.length(); i++){
+            long curr = Long.parseLong(num.substring(index, i+1));
+            
+            if((num.charAt(index) == '0') && index != i)
+                continue;
+            
+            if(index == 0){
+                backtracking(num, target, i+1, curr, curr, expr+curr);
+            }else{
+                // +
+                backtracking(num, target, i+1, prevSum + curr, curr, expr + '+' + curr);
+                // -
+                backtracking(num, target, i+1, prevSum - curr, -curr, expr + '-' + curr);
+                // *
+                backtracking(num, target, i+1, prevSum - prevAdd + prevAdd * curr, prevAdd * curr, expr + '*' + curr);
+            }
+        }
+    }
+}
