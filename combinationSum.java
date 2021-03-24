cf529f5618062cffae2d507fc9b8e0bc03f2afe4
dkeerthireddy <dharam.keerthi@gmail.com>
2021-03-23 21:47:47 -0600

    Completed Backtracking-1. Please review.

---
+++ combinationSum.java
@@ -0,0 +1,35 @@
+//Time Complexity: O(N^h) or O(N^(T/minEl)) where T is the target; minEl is the minimum element in the array
+//Space Complexity: O(h) or O(T/minEl) where h is the height of the tree
+
+//Code
+class Solution {
+    private List<List<Integer>> output = new ArrayList<>();
+    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        backtracking(candidates, target, 0, new ArrayList<>());
+        return output;
+        
+    }
+    private void backtracking(int[] candidates, int target, int index, List<Integer> path){
+        //base case
+        if(target<0){
+            return;
+        }
+        else if(target==0){
+            output.add(new ArrayList<>(path));
+        }
+        //logic
+        for(int i = index; i<candidates.length;i++){
+            path.add(candidates[i]);
+            
+            //recur
+            backtracking(candidates, target-candidates[i], i, path);
+            
+            //backtracking
+            //2,3 ->
+            path.remove(path.size() - 1);
+        }
+        
+        //backtracking
+    }
+}
