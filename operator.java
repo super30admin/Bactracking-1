bb9b02d00a5d0ffa303d5d51c8e9cc0b203135e2
SaurabhEdge <ss7586@rit.edu>
2021-05-26 15:37:48 -0400

    Completed backtracking-1  sprint

---
+++ operator.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Time Complexity : O(3^n)
+// Space Complexity :O(H)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+
+// Your code here along with comments explaining your approach
+
+
+public class operator {
+
+    List<String> res;
+    public List<String> addOperators(String num, int target) {
+        res= new ArrayList<>();
+        helper(num, target, "", 0, 0, 0);
+        
+    return res;
+    }
+    
+    
+    private void helper(String num, int target, String path, long calc, long tail, int index){
+       
+        if(index==num.length()){
+            if(calc==target){
+                res.add(path);    
+            }
+            return;
+        }
+
+        for(int i=index;i<num.length();i++){
+            long curr = Long.parseLong(num.substring(index, i+1));
+            //to avoid numbers formation without zero
+            if(index!=i && num.charAt(index)=='0') continue;
+            //for the first index
+            if(index ==0){
+                helper(num, target, curr+path,curr, curr, i+1);
+            }
+            else{
+                //+
+                helper(num,target,path+'+'+curr,curr+calc, curr,i+1);
+                //-
+                helper(num,target,path+'-'+curr,calc-curr,-curr,i+1 );
+                //*
+                helper(num,target,path+'*'+curr,calc-tail+curr*tail, curr*tail,i+1);
+            }
+        }
+    }
+    
+}
