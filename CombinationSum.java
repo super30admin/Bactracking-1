32cc2ad48edb672be787559f5adb8c9694d556a2
Sumit Chavan <chavansumit13@gmail.com>
2021-06-29 17:26:48 +0530

    Added recursion approach to CombinationSum

--- CombinationSum.java
+++ CombinationSum.java
@@ -15,32 +15,49 @@ public class CombinationSum {
     public List<List<Integer>> result = new ArrayList();
     public int[] nums;
     public int targetSum;
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
         nums = candidates;
         targetSum = target;
         List<Integer> list = new ArrayList();
-        helper(targetSum, 0, list);
+        helper_using_backtracking(targetSum, 0, list);
         return result;
 
     }
 
 
-    public void helper(int remainingSum, int start, List<Integer> list)
-    {
+    public void helper_using_backtracking(int remainingSum, int start, List<Integer> list) {
 
 
-        if(remainingSum < 0)
+        if (remainingSum < 0)
             return;
-        else if(remainingSum ==0)
+        else if (remainingSum == 0)
             result.add(new ArrayList<Integer>(list));
-        else
-        {
-            for(int i=start; i<nums.length;i++)
-            {
+        else {
+            for (int i = start; i < nums.length; i++) {
                 list.add(nums[i]);
-                helper( remainingSum - nums[i], i, list);
-                list.remove(list.size()-1);
+                helper_using_backtracking(remainingSum - nums[i], i, list);
+                list.remove(list.size() - 1);
+            }
+
+        }
+
+    }
+
+    public void helper_using_recursion(int remainingSum, int start, List<Integer> list) {
+
+
+        if (remainingSum < 0)
+            return;
+        else if (remainingSum == 0)
+            result.add(new ArrayList<Integer>(list));
+        else {
+            for (int i = start; i < nums.length; i++) {
+                List<Integer> deepCopy = new ArrayList(list);
+                deepCopy.add(nums[i]);
+                helper_using_recursion(remainingSum - nums[i], i, deepCopy);
+
             }
 
         }
