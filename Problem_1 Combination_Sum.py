5a617b999a3a0562e7e0f2a5ae324f54f296f710
sushmakallapur <sushma.kallapur@gmail.com>
2021-06-30 17:24:00 -0700

    Completed Backtracking-1

---
+++ Problem_1 Combination_Sum.py
@@ -0,0 +1,34 @@
+# // Time Complexity : Exponential
+# // Space Complexity : O(n)
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : No
+#
+#
+# // Your code here along with comments explaining your approach
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        if len(candidates) == 0:
+            return None
+        result = []
+        self.backtrack(candidates, target, 0, [], result)
+        return result
+
+    def backtrack(self, candidates, target, index, path, result):
+        # base
+        if target == 0:
+            result.append(path[:])
+            print(result)
+            # return result
+        # logic
+        if target < 0:
+            return
+        for i in range(index, len(candidates)):
+            if target >= candidates[i]:
+                path.append(candidates[i])
+                self.backtrack(candidates, target - candidates[i], i, path, result)
+                #if it returns because of any base condition, pop the last element
+                path.pop()
+
+
