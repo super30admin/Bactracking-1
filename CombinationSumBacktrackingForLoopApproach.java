a299d91ed4e107151e3b03c2e7a48b47293c26ea
Radhika Kulkarni <radhikankulkarni@gmail.com>
2021-07-04 22:40:13 +0530

    For loop approach

--- CombinationSumBacktrackingForLoopApproach.java
+++ CombinationSumBacktrackingForLoopApproach.java
@@ -1,29 +1,25 @@
 class Solution {
     List<List<Integer>> res;
-    private void helper(int[] candidates, int target, int ind, List<Integer> path) {
+    private void helper(int[] candidates, int target, List<Integer> path, int ind) {
         if(target < 0 || ind == candidates.length) {
             return;
         }
-
         if(target == 0) {
             res.add(new ArrayList<>(path));
             return;
         }
-
-        // choose
-        path.add(candidates[ind]);
-        helper(candidates, target - candidates[ind], ind, path);
-        path.remove(path.size() - 1);
-
-        // don't choose
-        helper(candidates, target, ind + 1, path);
+        for(int i = ind; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            helper(candidates, target - candidates[i], path, i);
+            path.remove(path.size() - 1);
+        }
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         res = new ArrayList<>();
         if(candidates == null || candidates.length == 0) {
             return res;
         }
-        helper(candidates, target, 0, new ArrayList<>());
+        helper(candidates, target, new ArrayList<>(), 0);
         return res;
     }
 }
