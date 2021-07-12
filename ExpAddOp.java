5531d877516cc1eceffc5524943e8d83a3f756ce
ngurung91 <nishantgurung.g@gmail.com>
2021-07-12 16:41:53 -0500

    Completed BackTracking-1

---
+++ ExpAddOp.java
@@ -0,0 +1,46 @@
+
+public class ExpAddOp {
+	    List<String> result;
+	    int targ;
+	    public List<String> addOperators(String num, int target) {
+	        targ = target;
+	        result = new ArrayList<>();
+	        helper(num, 0, 0, 0, "");
+	        return result;
+	    }
+	    
+	    private void helper(String num, int index, long calc, long tail, String path){
+	        //base
+	        if(index == num.length()){
+	            if(calc == targ){
+	                result.add(path);
+	                return;
+	            }
+	        }
+	        //logic
+	        for(int i = index; i < num.length();i++){
+	            //preceding zero
+	            if(num.charAt(index)== '0' && index != i) continue;
+	            long curr = Long.parseLong(num.substring(index, i+1));
+	            if(index==0){
+	                helper(num, i+1 , curr, curr, path + curr);
+	            }
+	            else{
+	                //+
+	                helper(num,i+1,calc+curr,curr,path+"+"+curr);
+	                
+	                //-
+	                helper(num,i+1,calc-curr,-curr,path+"-"+curr);
+	                //*
+	                helper(num,i+1,calc-tail+tail*curr,tail*curr,path+"*"+curr);
+	                
+	            }
+	      
+	        }
+	        
+	    }
+	    
+	    
+	    
+	    
+	}
