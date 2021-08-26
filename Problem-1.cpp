0bf0f07900720e692ca6efcbd7262a1844dbf409
Durgesh Patil <patildurgesh1998@gmail.com>
2021-08-26 12:35:37 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -32,3 +32,42 @@ public:
         helper(candidates, index, target-candidates[index], path);
     }
 };
+
+/*
+Time Complexity = Exponential
+Space Complexity = Exponential
+*/
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> path;
+        helper(candidates, 0, target, path);
+        return result;
+    }
+    void helper(vector<int>& candidates, int index, int target, vector<int>& path)
+    {
+        //base
+        if(index==candidates.size())
+            return;
+        if(target<0)
+            return;
+        if(target==0)
+        {
+            result.push_back(path);
+            return;
+        }
+            
+        //logic
+        //do not choose
+        helper(candidates, index+1, target, path);
+        
+        //choose
+        //action
+        path.push_back(candidates[index]);
+        //recurse
+        helper(candidates, index, target-candidates[index], path);
+        //backtrack
+        path.pop_back();
+    }
+};
