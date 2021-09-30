d6359fa45d4a3b2d119ed775c077c43817dae7de
Aashish Kumar <vats96ashu@gmail.com>
2021-09-30 01:50:18 -0500

    completed sprint-backtracking1

---
+++ expressionaddoperator.java
@@ -0,0 +1,39 @@
+
+public class expressionaddoperator {
+    List<String> result;
+
+    public List<String> addOperators(String num, int target) {
+        result = new ArrayList<>();
+        helper(num, target, 0, 0, 0, "");
+        return result;
+
+    }
+
+    private void helper(String num, int target, int index, long calc, long tail, String path) {
+        // base
+        if (index == num.length()) {
+            if (calc == target) {
+                result.add(path);
+            }
+            return;
+        }
+
+        // logic
+        for (int i = index; i < num.length(); i++) {
+            // preceding zero
+            if (i != index && num.charAt(index) == '0')
+                continue;
+            long curr = Long.parseLong(num.substring(index, i + 1));
+            if (index == 0) {
+                helper(num, target, i + 1, curr, curr, path + curr);
+            } else {
+                // +
+                helper(num, target, i + 1, calc + curr, curr, path + '+' + curr);
+                // -
+                helper(num, target, i + 1, calc - curr, -curr, path + '-' + curr);
+                // *
+                helper(num, target, i + 1, calc - tail + tail * curr, tail * curr, path + '*' + curr);
+            }
+        }
+    }
+}
