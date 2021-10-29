af3627768a668d444991ae32a8b47addf1ef5fb4
srinathsai <45191452+srinathsai@users.noreply.github.com>
2021-10-29 18:58:09 -0500

    Backtracking-1 completed please review
    Can you tell me time complexity of second sum?

---
+++ operators.java
@@ -0,0 +1,43 @@
+//Time complexity:-
+//Did it run on leetcode:- yes.
+//What was the problem you faced?:- took lot of time and got many errors.
+//your code with explanation:- Applying backtracking in which for every operation passing the parameter like that.
+
+
+class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> result = new ArrayList<>();
+        addOperators(num, target, result, 0, 0, 0, "");
+        return result;
+    }
+    
+    private void addOperators(String num, int target, List<String> result, int index, long value, long previous, String current) {
+        if(index == num.length()) {
+            if(value + previous == target) {
+                result.add(current);
+            }
+            return;
+        }
+        
+        int end = num.length();
+        if(num.charAt(index) == '0') {
+            end = index + 1;
+        }
+        
+        for(int i = index + 1; i <= end; i ++) {
+            long currentValue = Long.valueOf(num.substring(index, i));
+            
+            if(index == 0) {
+                addOperators(num, target, result, i, 0, currentValue, String.valueOf(currentValue));
+                continue;
+            } 
+            
+            // Add +
+            addOperators(num, target, result, i, value + previous, currentValue, current + "+" + currentValue);
+            // Add -
+            addOperators(num, target, result, i, value + previous, -currentValue, current + "-" + currentValue);
+            // Add *
+            addOperators(num, target, result, i, value, previous * currentValue, current + "*" + currentValue);
+        }
+    }
+}
