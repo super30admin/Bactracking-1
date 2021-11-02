d701e0085f2416dbc3b9ef1cdd3db167ceb287a1
panth1921 <panthdev96@gmail.com>
2021-11-02 14:53:43 -0400

    Bactracking-1

---
+++ combinationSum.swift
@@ -0,0 +1,31 @@
+// Time Complexity : 
+// Space Complexity : 
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+    var output = [[Int]]()
+    func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        if candidates == nil || candidates.count == 0 {
+            return []
+        }
+        var path = [Int]()
+        backtracking(candidates,target,0,&path)
+        return output
+        
+    }
+    func backtracking(_ candidates: [Int],_ target: Int,_ index: Int,_  path: inout [Int] ) {
+        if target < 0 {
+            return
+        }
+        if target == 0 {
+            var path = path
+            output.append(path)
+        }
+        for i in index..<candidates.count {
+            path.append(candidates[i])
+            backtracking(candidates, target - candidates[i],i,&path)
+            path.remove(at: path.count-1)
+        }
+    }
