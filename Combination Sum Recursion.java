199142fb8e192780b8cda29e07e40af54d11d2e2
aniket14thumar <aniket14.thumar@gmail.com>
2022-01-26 09:18:24 -0800

    Completed Combination Sum Recursion

---
+++ Combination Sum Recursion.java
@@ -0,0 +1,32 @@
+// Time Complexity : O(n)
+// Space Complexity : O(h)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        result = new ArrayList<>();
+        helper(candidates, 0, new ArrayList<>(), target);
+        return result;
+    }
+    
+    private void helper(int[] candidates, int index, List<Integer> path, int target){
+        // base case
+        if(target == 0){
+            result.add(path);
+            return;
+        }
+        
+        if(target < 0 || index == candidates.length) return;
+        
+        // logic
+        // choose
+        List<Integer> temp = new ArrayList<>(path);
+        temp.add(candidates[index]);
+        helper(candidates, index, temp, target - candidates[index]);
+        
+        // not choose
+        helper(candidates, index + 1, new ArrayList<>(path), target);
+    }
+}
