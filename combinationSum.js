dd720a14fe05b156aa4e2627c6dd53f719b5f371
Krina Patel <kp42942n@pace.edu>
2022-04-11 15:42:29 -0400

    Create combinationSum.js

---
+++ combinationSum.js
@@ -0,0 +1,93 @@
+// Time Complexity : O(n) where n is length of nums
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+
+//Bruteforce 0-1 recursion solution
+let result;
+var helper = function(candidates, target, index, path){
+    //base case
+    if(target === 0) {
+        result.push(path);
+        return;
+    }
+    if(target < 0 || index === candidates.length) return;
+
+    //logic
+    //no choose
+    helper(candidates, target, index + 1, [...path]);
+    //choose
+    path.push(candidates[index]);
+    helper(candidates, target - candidates[index], index, path); 
+}
+
+var combinationSum = function(candidates, target) {
+    result = [];
+    helper(candidates, target, 0, []);
+    return result;
+};
+
+//Backtrack 0-1 recursion solution
+/*
+let result;
+var helper = function(candidates, target, index, path){
+    //base case
+    if(target === 0) {
+        result.push([...path]);
+        return;
+    }
+    if(target < 0 || index === candidates.length) return;
+
+    //no choose
+    helper(candidates, target, index + 1, path);
+    
+    //choose
+    path.push(candidates[index]);
+    helper(candidates, target - candidates[index], index, path);
+    
+    //backtrack
+    path.pop();
+}
+
+var combinationSum = function(candidates, target) {
+    result = [];
+    helper(candidates, target, 0, []);
+    return result;
+};
+*/
+
+
+//Backtrack for loop based recursion solution
+/*
+let result;
+var helper = function(candidates, target, index, path){
+    //base case
+    if(target === 0) result.push([...path]);
+    if(target < 0) return;
+    
+    // action
+    for(let i = index; i < candidates.length; i++) {
+        //action
+        path.push(candidates[i]);
+        
+        //recusrion
+        helper(candidates, target - candidates[i], i, path);
+    
+        //backtracking
+        path.pop();
+    }
+    
+}
+
+var combinationSum = function(candidates, target) {
+    result = [];
+    helper(candidates, target, 0, []);
+    return result;
+};
+*/
