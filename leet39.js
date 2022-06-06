4d7afa8bd4aa98ce086cee96caa030d67e9c6164
Harsh <akshatrana2007@gmail.com>
2022-06-05 20:01:53 -0400

    Completed Bactracking-1

---
+++ leet39.js
@@ -0,0 +1,23 @@
+Time Complexity : O(N^2)
+Space Complexity : O(N^2)
+
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    const result = [];
+    
+    function permute(arr=[], sum=0, idx=0) {
+        if(sum > target) return;
+        if(sum === target) result.push(arr);
+        
+        for(let i = idx; i < candidates.length; i++) {
+            permute([...arr, candidates[i]], sum+candidates[i], i);
+        }
+    }
+    permute()
+    return result;
+};
