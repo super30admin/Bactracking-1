610f7864ec64fbea3232bbbb1c7f551858fa918c
prajay24 <67772724+prajay24@users.noreply.github.com>
2022-06-27 18:27:41 -0400

    first qquestion

---
+++ 39-CombinationSum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        
+        result = []
+        
+        def dfs(i, curr, total):
+            if total == target:
+                result.append(curr.copy())
+                return
+            if total > target or i>=len(candidates):
+                return
+            
+            curr.append(candidates[i])
+            dfs(i, curr, total+candidates[i])
+            curr.pop()
+            dfs(i+1, curr, total)
+            
+            
+        dfs(0, [],0)
+        return result
+
+        # space: o(2^target)
