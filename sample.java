65e71a7428d5fdc37fa0631ca6fe38c932415a75
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-30 09:22:51 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -168,3 +168,44 @@ class Solution {
 
 
 //COMBINATION SUM - FOR LOOP BASED RECURSION
+
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        result=new ArrayList<>();
+        //Null case
+        if(candidates.length==0 || candidates==null) return result;
+        helper(candidates, target, 0, new ArrayList<>());
+        return result;
+        
+        
+    }
+    //candidates
+    //Target
+    //i
+    //path
+    private void helper(int[] candidates, int target, int index,List<Integer> path)
+    {
+        //Base
+        if(target==0)
+        {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        if(target<0)
+        {
+            return;
+        }
+        //logic
+        for(int i=index;i<candidates.length;i++)
+        {
+            //Action
+            path.add(candidates[i]);
+            //Recurse
+            helper(candidates, target-candidates[i], i, path);
+            //Backtrack
+            path.remove(path.size()-1);
+            
+        }
+    }
+}
