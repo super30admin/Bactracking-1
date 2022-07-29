59f8bb275516037f196e546c189848d828e4ada7
saiteja1905 <sjavvaji@asu.edu>
2022-07-28 17:44:30 -0700

    Completed Backtracking-1 problems. Please verify and comment.

---
+++ AddOperators.py
@@ -0,0 +1,28 @@
+# Time Complexity => Exponential O(x^N)
+# Space Complexity => O(N)
+
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        n = len(num)
+        result = []
+
+        def helper(i,prev,curr,val,expr):
+            if i == n:
+                if val == target and curr==0:
+                    result.append(expr)
+                return
+
+            curr = curr*10 + int(num[i])
+
+            if curr>0:
+                helper(i+1,prev,curr,val,expr)
+
+            if not expr:
+                helper(i+1,curr,0,val+curr,str(curr))
+            else:
+                helper(i+1,curr,0,val+curr,expr+'+'+str(curr))
+                helper(i+1,-curr,0,val-curr,expr+'-'+str(curr))
+                helper(i+1,prev*curr,0,val-prev+prev*curr,expr+'*'+str(curr))
+
+        helper(0,0,0,0,"")
+        return result
