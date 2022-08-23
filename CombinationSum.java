9f31af7588f3e87c5c63fc5f6cd8db8c0ce8f8ad
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-08-22 17:05:09 -0700

    Update CombinationSum.java

--- CombinationSum.java
+++ CombinationSum.java
@@ -1,1 +1,47 @@
 
+
+
+
+//TC = 2^(n*k) 
+// n = nodes, k = amount
+// nodes is directly proportional to amount
+
+//Space = O(n^2) 
+// n nodes * O(n) Lists
+
+
+//Recursive Solution
+class Solution {
+    List<List<Integer>> result; // Global list [[],[]]
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        result = new ArrayList<>();
+        if(candidates == null){
+            return result;
+        }
+        helper(candidates, 0, target, new ArrayList<>());
+        return result;
+    }
+    
+    
+    private void helper(int[] candidates, int i, int amount, List<Integer> path){
+        //base
+        if(i==candidates.length||amount<0) return;
+        if(amount == 0){
+            result.add(path);
+            return;
+        }
+        //logic
+        
+        //DS in DS is a Reference
+        
+        //Not Choose
+        //sending into a sep List
+        helper(candidates, i+1, amount, new ArrayList<>(path));
+        
+        path.add(candidates[i]); //[2]
+        //Choose
+        helper(candidates, i, amount-candidates[i], new ArrayList<>(path));
+        
+        
+    }
+}
