54babd166ed5adbe1a99a6b9865b5c0859331be4
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-08-22 21:03:35 -0700

    Update CombinationSum.java

--- CombinationSum.java
+++ CombinationSum.java
@@ -45,3 +45,47 @@ class Solution {
         
     }
 }
+
+
+
+//First call choose and then Not Choose
+class Solution {
+    List<List<Integer>> result; // Global list [[],[]]
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        result = new ArrayList<>();
+        if(candidates == null){
+            return result;
+        }
+        helper(candidates, 0, target, new ArrayList<>());
+        return result;
+    }
+    private void helper(int[] candidates, int i, int amount, List<Integer> path){
+        //base
+        if(i==candidates.length||amount<0) return;
+        if(amount == 0){
+            result.add(path);
+            return;
+        }
+        //logic
+        
+        //DS in DS is a Reference
+        
+        
+        List<Integer> temp = new ArrayList<>(path);
+        
+        temp.add(candidates[i]);
+        
+        //Choose
+        helper(candidates, i, amount-candidates[i], temp);
+        
+        //Not Choose
+        //sending into a sep List
+        helper(candidates, i+1, amount, new ArrayList<>(path));
+        
+        
+        
+        
+        
+    }
+}
+
