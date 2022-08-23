8a74a5822274e41369ad989f88137bd0e347f92c
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-08-22 21:09:29 -0700

    Update CombinationSum.java

--- CombinationSum.java
+++ CombinationSum.java
@@ -89,3 +89,43 @@ class Solution {
     }
 }
 
+
+
+
+//Backtracking Concept
+//No need to create list every time.
+class Solution {
+    List<List<Integer>> result; // Global list [[],[]]
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        result = new ArrayList<>();
+        if(candidates == null){
+            return result;
+        }
+        helper(candidates, 0, target, new ArrayList<>());
+        return result;
+    }
+      private void helper(int[] candidates, int i, int amount, List<Integer> path){
+        //base
+        if(i==candidates.length||amount<0) return;
+        if(amount == 0){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        //logic
+        
+        //Not Choose
+        //sending into a sep List
+        helper(candidates, i+1, amount, path);
+        
+        //Choose  Action
+        path.add(candidates[i]); 
+        
+        //reuse
+        helper(candidates, i, amount-candidates[i], path); 
+        //backtrack
+        path.remove(path.size()-1);
+        
+    }
+}
+
+
