37dd0fb75e152d395fbd4216ebd13a12c2e687ab
sivakami-projects <126191544+sivakami-projects@users.noreply.github.com>
2023-04-11 20:50:38 -0700

    Update CombinationSum.java

--- CombinationSum.java
+++ CombinationSum.java
@@ -1,3 +1,19 @@
+/**
+    This algorithm creates a N-ary tree of depth T/M. 
+    T is the target value.
+    M is the minimum value in the candidates array. A smaller valued M means we keep adding that candidate to the combination and go deep into the tree until, combination sum >= 0.
+
+
+Time Complexity - O(N ^(T/M + 1)) In the below recursive approach, 
+                    the number of operations is loosely equal to the no. of nodes in N -ary tree. 
+                    Max depth is (T/M) 
+                    Max width or max no of children for each node is N (size of the candidates array)
+                    total no of nodes is (N^ (T/M + 1)).
+
+Space Complexity -  Auxillary space - all combinations are stored in list which is represented by every path from root to all leaves. 
+                    Max length of such path is max depth of the tree which is O(T/M).
+                    Stack space - again the max depth of the tree. O(T/M).
+*/
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         
