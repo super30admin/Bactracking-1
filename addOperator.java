53ca473c0d6dd933c925a57eeebc78536ed6db55
Bharath Barakam <bharath.barakam@ultra.me>
2023-06-12 01:16:31 -0700

    Finished Backtracking-1

---
+++ addOperator.java
@@ -0,0 +1,33 @@
+//Time Complexity: O(n * n)
+//Space Complexity: O(n)
+
+import java.util.*;
+
+class Solution {
+    List<List<Integer>> output;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        output = new ArrayList<>();
+        dfs(candidates, target, 0, new ArrayList<>());
+
+        return output;
+    }
+
+    private void dfs(int[] candidates, int target, int idx, List<Integer> res) {
+        
+        if(target == 0) {
+            System.out.println(res);
+            output.add(new ArrayList<>(res));
+            return;
+        }
+
+        if(idx >= candidates.length || candidates[idx] > target) {
+            return;
+        }
+
+        res.add(candidates[idx]);  
+        dfs(candidates, target - candidates[idx], idx, res);
+        res.remove(res.size()  - 1);
+        dfs(candidates, target, idx + 1, res);
+    }
+}
